8a5b69ebc58f08a2e07c0b161fb1e1ae85ba8253
jaspercurry <jaspercurry@gmail.com>
2016-11-14 23:32:12 -0500

    added js for moving img

--- js/index.js
+++ js/index.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
   console.log("Hey there ;)");
+
+  var mq = window.matchMedia( "(max-width: 768px)" );
+  var image = '<div class="col-xs-12 col-sm-7 col-md-5 move-image"><img class="img-responsive" src="./assets/layers.png" alt="" /></div>'
+
+  if (mq.matches) {
+    $(".move-image").remove()
+
+    $(".place-image").html(image)
+  }
 });
+
+$(window).resize(function() {
+  var mq = window.matchMedia( "(max-width: 768px)" );
+  var image = '<div class="col-xs-12 col-sm-7 col-md-5 move-image"><img class="img-responsive" src="./assets/layers.png" alt="" /></div>'
+
+  if (mq.matches) {
+    $(".move-image").remove()
+    $(".place-image").html(image)
+  } else {
+    $(".move-image").remove()
+    $(".return-image").prepend(image)
+  }
+
+})
 
 $(window).scroll(function() {
     if($(this).scrollTop() < 100) {
